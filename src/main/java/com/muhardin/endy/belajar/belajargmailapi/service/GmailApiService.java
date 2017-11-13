195092f59445c8ec05ac1decc6a8e0611ea07b7e
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-13 11:21:56 +0700

    kirim email dengan GMail API

--- src/main/java/com/muhardin/endy/belajar/belajargmailapi/service/GmailApiService.java
+++ src/main/java/com/muhardin/endy/belajar/belajargmailapi/service/GmailApiService.java
@@ -9,21 +9,32 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Base64;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 @Service
 public class GmailApiService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GmailApiService.class);
 
     private static final List<String> SCOPES =
             Arrays.asList(GmailScopes.GMAIL_SEND);
@@ -73,6 +84,28 @@ public class GmailApiService {
     }
 
     public void kirimEmail(String from, String to, String subject, String content){
+        try {
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props, null);
 
+            InternetAddress destination = new InternetAddress(to);
+            MimeMessage email = new MimeMessage(session);
+            email.setFrom(new InternetAddress(gmailUsername, from));
+            email.addRecipient(javax.mail.Message.RecipientType.TO, destination);
+            email.setSubject(subject);
+            email.setContent(content, "text/html; charset=utf-8");
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            email.writeTo(buffer);
+            byte[] bytes = buffer.toByteArray();
+            String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+            Message message = new Message();
+            message.setRaw(encodedEmail);
+
+            message = gmail.users().messages().send("me", message).execute();
+            LOGGER.info("Email {} from {} to {} with subject {}", message.getId(), from, destination, subject);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
--- src/test/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplicationTests.java
+++ src/test/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplicationTests.java
@@ -1,16 +1,28 @@
 package com.muhardin.endy.belajar.belajargmailapi;
 
+import com.muhardin.endy.belajar.belajargmailapi.service.GmailApiService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BelajarGmailApiApplicationTests {
 
+	@Autowired private GmailApiService gmailApiService;
+
 	@Test
-	public void contextLoads() {
+	public void testKirimEmail() {
+		gmailApiService.kirimEmail(
+				"Belajar GMail API",
+				"endy.muhardin@gmail.com",
+				"Email Percobaan" + LocalDateTime.now(),
+				"Ini email percobaan dikirim dari aplikasi"
+				);
 	}
 
 }
