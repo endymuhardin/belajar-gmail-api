f0d123c0e8b2cfbf04f9691578286981fb4173eb
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-13 13:02:03 +0700

    email dengan HTML template

--- src/main/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplication.java
+++ src/main/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplication.java
@@ -1,12 +1,20 @@
 package com.muhardin.endy.belajar.belajargmailapi;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.MustacheFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BelajarGmailApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BelajarGmailApiApplication.class, args);
+	}
+
+	@Bean
+	public MustacheFactory mustacheFactory(){
+		return new DefaultMustacheFactory();
 	}
 }
--- src/test/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplicationTests.java
+++ src/test/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.muhardin.endy.belajar.belajargmailapi;
 
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import com.muhardin.endy.belajar.belajargmailapi.service.GmailApiService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,13 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.StringWriter;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BelajarGmailApiApplicationTests {
 
 	@Autowired private GmailApiService gmailApiService;
+	@Autowired private MustacheFactory mustacheFactory;
 
 	@Test
 	public void testKirimEmail() {
@@ -23,6 +29,23 @@ public class BelajarGmailApiApplicationTests {
 				"Email Percobaan" + LocalDateTime.now(),
 				"Ini email percobaan dikirim dari aplikasi"
 				);
+	}
+
+	@Test
+	public void testKirimEmailDenganTemplate(){
+		Mustache templateEmail = mustacheFactory.compile("templates/welcome.html");
+		Map<String, String> data = new HashMap<>();
+		data.put("nama", "Endy Muhardin");
+		data.put("pesan", "Anda telah terdaftar di Aplikasi Notifikasi. Silahkan tunggu instruksi selanjutnya");
+
+		StringWriter output = new StringWriter();
+		templateEmail.execute(output, data);
+
+		gmailApiService.kirimEmail(
+				"Belajar GMail API",
+				"endy.muhardin@gmail.com",
+				"Percobaan Mustache Template",
+				output.toString());
 	}
 
 }
