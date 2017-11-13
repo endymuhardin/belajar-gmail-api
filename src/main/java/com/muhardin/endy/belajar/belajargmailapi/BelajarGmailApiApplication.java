2ca8082b55a9cab3bc955e4e3512d566aa56975b
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-13 10:12:48 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar.belajargmailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarGmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarGmailApiApplication.class, args);
+	}
+}
