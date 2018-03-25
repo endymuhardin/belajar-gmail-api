8f49b24358ba9b0a757da9ee683b330ee396aac5
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-26 01:00:46 +0700

    download file credential kalau lagi di heroku

--- src/main/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplication.java
+++ src/main/java/com/muhardin/endy/belajar/belajargmailapi/BelajarGmailApiApplication.java
@@ -10,10 +10,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.*;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BelajarGmailApiApplication {
@@ -23,6 +29,10 @@ public class BelajarGmailApiApplication {
 	}
 
 	private static final String CLIENT_SECRET_JSON_FILE = "client_secret.json";
+	private static final String STORED_CREDENTIAL_FILE = "StoredCredential";
+
+	@Value("${GMAIL_CREDENTIAL_FILE_SERVER}")
+	private String credentialFileServer;
 
 	@Value("${gmail.folder}")
 	private String dataStoreFolder;
@@ -42,4 +52,33 @@ public class BelajarGmailApiApplication {
 		return GoogleClientSecrets.load(jsonFactory(),
 						new InputStreamReader(new FileInputStream(dataStoreFolder + File.separator + CLIENT_SECRET_JSON_FILE)));
 	}
+
+	@Bean @Profile("heroku")
+	public GoogleClientSecrets downloadClientSecrets() throws Exception {
+		downloadCredentialsFile(CLIENT_SECRET_JSON_FILE);
+		downloadCredentialsFile(STORED_CREDENTIAL_FILE);
+		return GoogleClientSecrets.load(jsonFactory(),
+				new InputStreamReader(new FileInputStream(dataStoreFolder + File.separator + CLIENT_SECRET_JSON_FILE)));
+	}
+
+	private void downloadCredentialsFile(String filename) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(
+				new ByteArrayHttpMessageConverter());
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+		ResponseEntity<byte[]> response = restTemplate.exchange(
+				credentialFileServer + "/" + filename,
+				HttpMethod.GET, entity, byte[].class, "1");
+
+		if (response.getStatusCode() == HttpStatus.OK) {
+			Files.createDirectories(Paths.get(dataStoreFolder));
+			Files.write(Paths.get(dataStoreFolder + File.separator + filename), response.getBody());
+		}
+	}
+
 }
